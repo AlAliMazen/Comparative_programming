9b0c4e9b567445b2a13ddfc8e01abd1b1be9faa0
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-01-29 18:43:16 +0100

    Feat: creating Objects

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -104,3 +104,46 @@ function getResult(){
 }
 
 
+//container are just to store different types of data, we use 3 key word, let, const and var 
+/*
+* simply calling the varibale by its name to print it out.
+*/
+
+let fruits = ['apple', 'orange', 'banana'];
+let myHtml = '<ol>';
+
+for (let i = 0; i < fruits.length; i++) {
+  myHtml = myHtml + '<li>' + fruits[i] + '</li>';
+}
+myHtml += '</ol>';
+
+console.log(myHtml);
+
+
+console.log("******************** Detecting data types using typeof operator as operator and as a function ******************");
+console.log(typeof "this is a string ");
+console.log(typeof 123);
+console.log(typeof 3.4);
+console.log(typeof true);
+console.log(typeof([1,3,5,6,7]));
+console.log({Model:"BMW",Year:2001, horsePower:320 });
+
+let nr_tst=parseFloat(window.prompt("Enter numbers to get result: "));
+console.log("Result is: "+ nr_tst);
+console.log("number to binnary: "+ (Math.abs(51247).toString(2)));
+
+let nr_a=1.02564;
+let nr_b=1.03;
+let res=((nr_a*10+nr_b*10)/10);
+console.log("End Result is: "+res);
+
+//creating an object 
+let human={
+    fullname:"",
+    sex:"",
+    dateOfBirth:""
+}
+
+let  johnSmith=new human();
+johnSmith.fullname="John Smith";
+console.log(Object.hasOwnProperty('age'));
